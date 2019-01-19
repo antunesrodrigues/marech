7aa4f81be74b0ee2f446faec5ebb23001a3b1363
Gabriel Rodrigues <jipacoding@gmail.com>
2019-01-19 09:45:43 -0400

    Eslint update

--- src/marechal/marechal-by-text.js
+++ src/marechal/marechal-by-text.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-cond-assign */
 // Required libs
 const path = require('path');
 const util = require('../../lib/all');
@@ -7,24 +8,24 @@ const marechalCore = require('./marechal-core');
 const marechalByData = (originalData, configs) => {
   // Keep original data and set it to another
   let finalData = originalData;
-  
+
   // Go to each <Marech@...> tag
   const marechExp = util.regExp.marechTag;
 
-  while(finalData.match(marechExp) !== null) {
+  while (finalData.match(marechExp) !== null) {
     // By eslint, 'match' need be defined
     let match;
-    while((match = marechExp.exec(finalData)) !== null) {
+    while ((match = marechExp.exec(finalData)) !== null) {
       // Normalize if user use break-line inside marech tag
       match[0] = match[0].replace(/(\r|\t|\n| {2,})/g, ' ');
 
       // Text before MarechTag definition
       const beforeTag = util.marechHelpers.beforeAndAfterMatch.before(finalData, match);
       // Before tag + tag
-      const beforeAndTag  = beforeTag + match[0];
+      const beforeAndTag = beforeTag + match[0];
       // Text after MarechTag definition
       const afterTag = util.marechHelpers.beforeAndAfterMatch.after(finalData, match);
-      
+
       // Get Teleg name
       const name = util.marechHelpers.nameOrProps('name', match[0]);
       // Get Properties definition
@@ -34,12 +35,14 @@ const marechalByData = (originalData, configs) => {
 
 
       // Find teleg file from Marech@... definition
-      const telegFile = util.marechHelpers.findTelegName(name, configs);      
+      const telegFile = util.marechHelpers.findTelegName(name, configs);
       // Read the teleg
-      const originalMarechTeleg = util.disk.file.readFile(path.join(configs.telegs.path, telegFile));
+      const telegPath = path.join(configs.telegs.path, telegFile);
+      const originalMarechTeleg = util.disk.file.readFile(telegPath);
 
       // Split to content and args
-      const {marechTeleg, args, defaultTelegArgs} = util.marechHelpers.execObj(originalMarechTeleg, props, preProps);
+      const splitedItens = util.marechHelpers.execObj(originalMarechTeleg, props, preProps);
+      const { marechTeleg, args, defaultTelegArgs } = splitedItens;
 
 
       // MarechalCORE
--- src/marechal/marechal-core.js
+++ src/marechal/marechal-core.js
@@ -2,26 +2,30 @@ const resolveFunction = require('../../lib/functions/function-js');
 
 const marechCore = (telegHtml, args, defaultArgs = {}) => {
   const telegWithoutMarech = telegHtml;
-  
+
   // Define final teleg
   let finalTeleg = telegWithoutMarech;
 
   // Verify if args are object
-  if(Object.prototype.toString.call(args) === '[object Object]') {
+  if (Object.prototype.toString.call(args) === '[object Object]' && args.length) {
     const directives = finalTeleg.match(/{( *).+( *)}/g);
-    for (let i in directives) {
+
+    for (let i = 0; i < directives.length; i += 1) {
       const directive = directives[i];
       const directiveName = directive.replace(/{|}/g, '');
-      
-      const replace = (args[directiveName] ? args[directiveName] : (defaultArgs[directiveName] ? defaultArgs[directiveName] : ''));
-      
+
+      const argsDirective = args[directiveName];
+      const defaultDirective = defaultArgs[directiveName];
+
+      const replace = argsDirective || defaultDirective || '';
+
       finalTeleg = finalTeleg.replace(new RegExp(`${directive}`, 'g'), replace);
     }
   }
-  
+
   const rjs = finalTeleg.match(/JS\(([^](?!(>)))*\)/g);
-  if(rjs) {
-    rjs.forEach(e => {
+  if (rjs) {
+    rjs.forEach((e) => {
       finalTeleg = finalTeleg.replace(e, resolveFunction(e));
     });
   }
